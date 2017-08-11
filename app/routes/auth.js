0770a63ee311743276ac9c966dcc153f30d7ce47
Sarthak Patidar <sarthakpatidar7@gmail.com>
2017-08-12 01:08:10 +0530

    dum laga k haisha

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,3 +1,7 @@
+var Doc = require('../models/doctor.js');
+var Pat = require('../models/patient.js');
+var val = Math.floor(1000 + Math.random() * 9000);
+var OTP;
 
   function buildResultSet(docs){
     var result = [];
@@ -15,16 +19,76 @@ module.exports = function(router, passport,io){
   });
   
   router.get('/login', function(req, res){
-
-      res.render('auth_files/signin.ejs', { message: req.flash('loginMessage') });
-
+      res.render('auth_files/login.ejs', { message: req.flash('loginMessage') });
   });
 
-  router.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/auth/login',
-    failureFlash: true
-  }));
+  router.post('/login', function(req,res){
+     if(req.body.otp){
+        if(req.body.otp == OTP){
+               res.send('Adhar No.: '+req.body.uid+' Otp:'+ req.body.otp);
+        }else{
+               res.send('dont make chutiya');
+        }
+     }else{
+        console.log(req.body.phno);
+        var client = require('twilio')(
+          'ACc5a3228883866d4142e93f27f13fe7e2',
+          '4c68b2104333d707520c386a799fe520'
+        );
+
+        client.messages.create({
+          from: '+12052877912',
+          to: '+91'+req.body.phno,
+          body: "Your OTP (one time password) is "+val
+        }, function(err, message) {
+            if(err) {
+                console.error(err.message);
+            }else{
+              console.log('message sent');
+              console.log('Aadhar id: '+req.body.uid)
+            Doc.findOne({'uid': req.body.uid },function(err,user){
+               if(err){
+                 throw err;
+               }
+               if(user){
+                  OTP = val;
+                  user.otp = val;
+                  user.save(function(err){
+                     if(err){
+                       throw err;
+                     }
+                     else{
+                       console.log('otp saved');
+                     }
+                  });
+               }
+               else{
+                   Pat.findOne({'uid': req.body.uid },function(err,user){
+                      if(err){
+                        throw err;
+                      }
+                      if(user){
+                          user.otp = val;
+                          user.save(function(err){
+                             if(err){
+                               throw err;
+                             }
+                             else{
+                               console.log('otp saved');
+                             }
+                          });
+                      }
+                      else{
+                         res.send('no person found');
+                      }
+                   })
+               }
+            });
+            }
+        });       
+     }
+     
+  });
 
  //localhost:8080/auth/signup
   router.get('/signup_doc', function(req, res){
@@ -33,7 +97,7 @@ module.exports = function(router, passport,io){
       io.on('connection',function(socket){
         console.log('connection established');
       });
-      res.render('auth_files/doc_signup.ejs',{ message: req.flash('signupMessage')} );
+      res.render('auth_files/doctor.ejs',{ message: req.flash('signupMessage')} );
       j= j+1;      
     }
   });
@@ -44,32 +108,106 @@ module.exports = function(router, passport,io){
       io.on('connection',function(socket){
         console.log('connection established');
       });
-      res.render('auth_files/pat_signup.ejs',{ message: req.flash('signupMessage')} );
+      res.render('auth_files/patient.ejs',{ message: req.flash('signupMessage')} );
       j= j+1;      
     }
 
   });
 
 
-  router.post('/signup_pat', passport.authenticate('local-signup', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/auth/signup_pat',
-    failureFlash: true
-  }));
+  router.post('/signup_pat',function(req,res){
+      // He is a Patient
+// Search if the member is already present
+      Doc.findOne({'uid': req.body.Aadh },function(err,user){
+         if(err){
+           throw err;
+         }
+         if(user){
+            res.redirect('/mem_present');
+         }
+         else{
+             Pat.findOne({'uid': req.body.Aadh },function(err,user){
+                if(err){
+                  throw err;
+                }
+                if(user){
+                   res.redirect('/mem_present');
+                }
+                else{
 
-  router.post('/signup_doc', passport.authenticate('local-signup', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/auth/signup_doc',
-    failureFlash: true
-  }))
+                    var newUser = new Pat();
 
-  /*router.get('/setup/stud_setup',function(req,res){
-         res.render('setup/stud_setup.ejs', { user: req.user});
+                    console.log(req.body);
+                    newUser.uid = req.body.Aadh;
+                    newUser.Name = req.body.username_pat;
+                    newUser.Blood = req.body.blood_group;
+                    newUser.Gender = req.body.gender;
+                    newUser.Location = req.body.Address+' '+req.body.city;
+                    newUser.Dob = req.body.dob; 
+
+                    newUser.save(function(err){
+                      if(err){
+                        throw err
+                      }else{
+                        console.log('patient saved');
+                      }
+                    });  
+
+                    res.redirect('/dashboard');                  
+                }
+             })
+         }
+      });    
   });
 
-  router.get('/setup/prof_setup',function(req,res){
-         res.render('setup/prof_setup.ejs', { user: req.user});
-  }); */
+  router.post('/signup_doc',function(req,res){
+      // He is a Doctor
+      console.log(req.body.Aadh);
+      Doc.findOne({'uid': req.body.Aadh },function(err,user){
+         if(err){
+           throw err;
+         }
+         if(user){
+            res.redirect('/mem_present');
+         }
+         else{
+             Pat.findOne({'uid': req.body.Aadh },function(err,user){
+                if(err){
+                  throw err;
+                }
+                if(user){
+                   res.redirect('/mem_present');
+                }
+                else{
+                    var newUser = new Doc();
+                    
+                    console.log(req.body);
+                    newUser.uid = req.body.Aadh;
+                    newUser.Gender = req.body.gender;
+                    newUser.Name = req.body.username_doc;
+                    newUser.Location = req.body.clc_add;
+                    newUser.Pos = req.body.position;
+                    newUser.Spcl = req.body.spcl;
+                    newUser.Deg = req.body.deg;
+                    newUser.Exp = req.body.exp;
+                    newUser.Fee = req.body.clc_fee;
+                    newUser.Contact = req.body.contact;
+
+                    newUser.save(function(err){
+                      if(err){
+                        throw err
+                      }else{
+                        console.log('doctor saved');
+                      }
+                    }); 
+
+                    res.redirect('/dashboard');
+
+                }
+             })
+         }
+      });      
+  });
 
   router.get('/logout', function(req, res){
     req.logout();
